94b6867c665990b86961ef46184f756b214508c7
Sujal Agarwal <sujalagarwal.nitdgp@gmail.com>
2026-04-04 03:35:12 +0530

    Blog model created and migrated to admin

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # models
-from .models import Category
+from .models import Category, Blog
 
 # Register your models here.
 admin.site.register(Category)
+admin.site.register(Blog)
--- blogs/models.py
+++ blogs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -9,6 +10,36 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
-        
+
     def __str__(self):
         return self.category_name
+
+
+STATUS_CHOICES = (
+    (0, 'Draft'),
+    (1, 'Published'),
+)
+
+class Blog(models.Model): 
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+    content = models.TextField(max_length=2000)
+    featured_image = models.ImageField(upload_to='uploads/%Y/%m/%d')
+    # in uploads sorted with year, month, day
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    short_desc = models.TextField(max_length=500)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    # for creating drop-down menu
+    status = models.IntegerField(choices=STATUS_CHOICES, default=0)
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Blog'
+        verbose_name_plural = 'Blogs'
+
+    def __str__(self):
+        return self.title
