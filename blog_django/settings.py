9d75932d0544f8b32d5ac4adfbc717e5747e306b
Sujal Agarwal <sujalagarwal.nitdgp@gmail.com>
2026-04-06 19:10:32 +0530

    Featured posts display and image display on landing page bug fixed

--- blog_django/settings.py
+++ blog_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,5 +122,5 @@ STATICFILES_DIRS = [
     'blog_django/static'
 ]
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
